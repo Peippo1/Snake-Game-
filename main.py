ba123fe2a3a6cabe6443abdd9c6fb9bcbefb41d9
Tim Finch <tim.o.finch@gmail.com>
2021-12-14 17:35:24 +0000

    Screen added

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
