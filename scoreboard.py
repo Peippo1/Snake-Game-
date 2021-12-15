7b9a02f1c2cda5440a419cfb37618b78801a4100
Tim Finch <tim.o.finch@gmail.com>
2021-12-15 09:47:14 +0000

    scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+# Constants used for placement
+ALIGNMENT = "center"
+FONT = "Courier ", 24, "normal"
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
