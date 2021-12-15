fad2907b96fcbbc06ce644ad63de0483ee26c644
Tim Finch <tim.o.finch@gmail.com>
2021-12-15 09:29:23 +0000

    onkey func

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
+
+# game_is_on = False
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
