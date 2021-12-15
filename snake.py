51af1870ac01456c1f0a08862ee90b09d0a1ae84
Tim Finch <tim.o.finch@gmail.com>
2021-12-15 09:54:16 +0000

    food/scoreboard/snake.py

---
+++ snake.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+# Starting positions are as constants
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
